00f7b4ee4117f2f61f812f07a8774572ae8eb56f
key <1612730560@qq.com>
2017-09-18 17:34:17 +0800

    异步检验ip有效性

--- src/refresh.py
+++ src/refresh.py
@@ -8,50 +8,66 @@
 from proxy_fuzz import fuzz_all, proxy_is_useful
 from redis_client import RedisClient
 from multiprocessing.dummy import Pool as ThreadPool
+import asyncio
 
 
 class ProxyRefresh():
 
-    def __init__(self, pool_num=10):
-        self.https_proxy = RedisClient('https_proxy')
-        self.http_proxy = RedisClient('http_proxy')
-        self.proxy_list = list(fuzz_all())
-        self.pool_num = pool_num
+    def __init__(self, proxy_type='https'):
+        if proxy_type == 'https':
+            self.redis_handler = RedisClient('https_proxy')
+        elif proxy_type == 'http':
+            self.redis_handler = RedisClient('http_proxy')
+        else:
+            raise Exception('type must be https or http')
+        self.proxy_type = proxy_type
+        self.proxy_pool = set([*fuzz_all(), *self.redis_handler.get_all()])
 
-    def refresh_all(self):
-        self.refresh_https()
-        self.refresh_http()
-
-    def refresh_https(self):
-        pool = ThreadPool(self.pool_num)
-        pool.map(self.valid_https_task, self.https_proxy.get_all())
-        pool.map(self.valid_https_task, self.proxy_list)
+    def refresh(self, pool_num=10):
+        pool = ThreadPool(pool_num)
+        pool.map(self.valid_ip, self.proxy_pool)
         pool.close()
         pool.join()
 
-    def refresh_http(self):
-        pool = ThreadPool(self.pool_num)
-        pool.map(self.valid_http_task, self.http_proxy.get_all())
-        pool.map(self.valid_http_task, self.proxy_list)
-        pool.close()
-        pool.join()
+    def refresh_in_async(self):
+        asynctask = AsyncTask()
+        for ip in self.proxy_pool:
+            asynctask.add_task(self.valid_ip, ip)
+        asynctask.run()
 
-    def valid_http_task(self, p):
-        if proxy_is_useful(p, 'http'):
-            self.http_proxy.add(p)
+    def valid_ip(self, ip):
+        if proxy_is_useful(ip, self.proxy_type):
+            self.redis_handler.add(ip)
         else:
-            self.http_proxy.delete(p)
+            self.redis_handler.delete(ip)
 
-    def valid_https_task(self, p):
-        if proxy_is_useful(p, 'https'):
-            self.https_proxy.add(p)
-        else:
-            self.https_proxy.delete(p)
+
+class AsyncTask():
+
+    def __init__(self):
+        self.__tasks = []
+        self.__loop = asyncio.get_event_loop()
+
+    def add_task(self, func, *args, **kwargs):
+        task = asyncio.ensure_future(self.run_async(func, *args, **kwargs))
+        self.__tasks.append(task)
+
+    async def run_async(self, func, *args, **kwargs):
+        future1 = self.__loop.run_in_executor(None, func, *args, **kwargs)
+        return await future1
+
+    def run(self):
+        res = self.__loop.run_until_complete(asyncio.wait(self.__tasks))
+        self.__tasks = []
+        return res
+
+    def __del__(self):
+        self.__loop.close()
 
 
 def main():
     proxy_refresh = ProxyRefresh()
-    proxy_refresh.refresh_all()
+    proxy_refresh.refresh_in_async()
 
 
 def run_in_shcedule():
@@ -59,6 +75,7 @@ def run_in_shcedule():
     sched = BlockingScheduler()
     sched.add_job(main, 'interval', minutes=10)
     sched.start()
+
 
 if __name__ == '__main__':
     main()
